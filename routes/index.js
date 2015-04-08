60179a55e3f50ede59282288f28dc4174b06018d
wangxiaochi <wangxiaochi@baidu.com>
2015-04-08 19:05:53 +0800

    登录成功

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,31 @@ router.get('/', function(req, res, next) {
 router.get('/new/question', function(req, res, next) {
   res.render('index', { title: '提一个问题' });
 });
+
 router.get('/login', function(req, res, next) {
-  res.render('index', { title: '登录' });
+  res.render('login', { title: '登录' });
+});
+router.post('/login', function(req, res, next) {
+  var params = [req.body.email, req.body.password];
+  router.db.query('SELECT *from user where email=? and password=? limit 1', params, function(err, rows, fields) {
+    if (err) throw err;
+    if (rows.length === 0) {
+      console.log('no user');
+    } else {
+      var user = rows[0];
+      console.log(user.id, 'login');
+      var sess = req.session
+      sess.uid = user.id;
+      res.render('login', { title: '登录成功' });
+    }
+  });
+  // res.render('login', { title: '登录' });
 });
 
 router.get('/register', function(req, res, next) {
   res.render('register', { title: '注册' });
 });
 router.post('/register', function(req, res, next) {
-  console.log(req.body)
   var mysql      = require('mysql');
   var connection = mysql.createConnection({
     host     : 'localhost',
@@ -26,11 +42,7 @@ router.post('/register', function(req, res, next) {
     database : 'know_more'
   });
 
-  // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  //   if (err) throw err;
-  //   console.log('The solution is: ', rows[0].solution);
-  // });
-  // var data = {email: req.params.}
+  // todo encrypt
   connection.query('INSERT INTO user SET ?', req.body, function(err, result) {
     if (err) throw err;
 
